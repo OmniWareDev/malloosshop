c6b7284810954c107b797ed4be0434aa23ebf3b6
@quacko.exe <darian.bayan2@gmail.com>
2025-08-13 12:53:27 -0700

    Updated server.js
    Updated to Hosting standards

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(__dirname)); // Serve static files from root directory
 app.use(express.json());
 
 const invPath = path.join(__dirname, 'inventory.json');
